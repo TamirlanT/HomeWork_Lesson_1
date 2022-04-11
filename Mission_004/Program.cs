c5fd066c3cd9fec85bcc0f3c48c5316f4fde700d
Tamirlan <tuarshintamir@gmail.com>
2022-04-11 20:14:08 +0500

    Das not complete HW

---
+++ Mission_004/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётныечисла от 1 до N. 
+int n, i;
+string n = Console.ReadLine();
+
+i = 1;
+while (i <= n)
+{
+    if (i % 2 ==0)
+        Console.WriteLine($"{i},");
+    else
+        i++;
+i++;
+}
+Console.WriteLine($"{i}.");
