7c065c4bca7507d38f1ac5c888b8f72452ca598b
Tamirlan <tuarshintamir@gmail.com>
2022-04-11 17:36:47 +0500

    New Mission_001

---
+++ Mission_001/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+int a = 5;
+int b = 7;
+if (a > b)
+Console.WriteLine($" {a} ");
+else
+Console.WriteLine($" {b} ");
+
+int c = 2;
+int d = 10;
+if (c > d)
+Console.WriteLine($"{c}");
+else
+Console.WriteLine($"{d}");
+
+int i = -9;
+int f = -3;
+if (i > f)
+Console.WriteLine($"{i}");
+else
+Console.WriteLine($"{f}");
