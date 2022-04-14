2769b4d00a8f56b1d8d6b0d534c97ca001900c53
Tamirlan <tuarshintamir@gmail.com>
2022-04-14 18:34:56 +0500

    Comleted Mission_004

--- Mission_004/Program.cs
+++ Mission_004/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётныечисла от 1 до N. 
 int n, i;
-string n = Console.ReadLine();
-
+n = 7;
 i = 1;
-while (i <= n)
+while (i < n)
 {
     if (i % 2 ==0)
-        Console.WriteLine($"{i},");
+        Console.Write($"{i}");
     else
-        i++;
-i++;
-}
-Console.WriteLine($"{i}.");
+    
+        Console.Write(" ");
+       
+    i++;    
+}     
