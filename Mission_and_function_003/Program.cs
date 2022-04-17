35389f45e7e5c1f08d36b1a508e21076295450d1
Tamirlan <tuarshintamir@gmail.com>
2022-04-17 22:17:17 +0500

    Don't copmplete Massive_003

---
+++ Mission_and_function_003/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int [] array = new Random().Next(1 , 10000);
+
+int index = 0;
+int n = array.Length;
+while (index < 2);
+    index++;
+Console.WriteLine($"{index}");
+
