6d494983d6b4e67a3ad7c306dd72ebb189237278
Tamirlan <tuarshintamir@gmail.com>
2022-04-26 14:38:06 +0500

    Massive_and_func_004 complete

---
+++ Massive_and_function_004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+Console.WriteLine("Введи число от 1 до 7");
+string  number = Console.ReadLine();
+
+if (number == "6") 
+    Console.WriteLine("Выходной");
+if (number == "7")
+    Console.WriteLine("Выходной");
+if(number == "1") 
+    Console.WriteLine("Тебе нужно работать");
+if(number == "2") 
+    Console.WriteLine("Тебе нужно работать");
+if(number == "3") 
+    Console.WriteLine("Тебе нужно работать");
+if(number == "4") 
+    Console.WriteLine("Тебе нужно работать");
+if(number == "5") 
+    Console.WriteLine("Тебе нужно работать");
