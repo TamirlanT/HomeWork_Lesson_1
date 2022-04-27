d9996d816d9a6674dcf7ddfc9d2dcca06cd033d0
Tamirlan <tuarshintamir@gmail.com>
2022-04-27 15:46:32 +0500

    HM_3 N^3 comlete

---
+++ Massive_and_function_Lesson_3_HM_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Возьмем число 5 и выведем таблицу кубов от 1 до 5");
+int N = 5;
+int i = 1;
+int sqr;
+while (i<=N)
+{
+    sqr = i*i*i;
+    Console.WriteLine($"{sqr}",sqr);
+    i++;
+}
+
+// число 3
+double n = 3;
+double index = 1;
+double Sqr;
+while (index <= n)
+{
+    Sqr = Math.Pow(index, 3);
+    Console.WriteLine($"{Sqr}", Sqr);
+    index++;
+}
